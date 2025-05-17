db1970ab6d10e8dc661c0329a868d85ca59b6f45
Rajesh <rajeshdhonde515@gmail.com>
2025-05-17 13:20:42 +0530

    modified by rajesh

--- src/main/java/in/ashokit/service/TicketServiceImpl.java
+++ src/main/java/in/ashokit/service/TicketServiceImpl.java
@@ -23,7 +23,7 @@ public class TicketServiceImpl implements TicketService{
 	
 		Ticket t=new Ticket();
 		
-		//Rajesh Dhonde
+		//Rajesh Dhonde-----------
 		Random r=new Random();
 		int id=r.nextInt(10);
 		t.setTicketNum(id);
