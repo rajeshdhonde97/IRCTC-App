3d01a80ea7325db4e333cd80c60911120df9ce4d
rajeshdhonde97 <93151364+rajeshdhonde97@users.noreply.github.com>
2025-05-17 13:30:46 +0530

    changing by sarthak

--- src/main/java/in/ashokit/service/TicketService.java
+++ src/main/java/in/ashokit/service/TicketService.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import in.ashokit.bindings.Passenger;
 import in.ashokit.bindings.Ticket;
 
+//this is my service
 public interface TicketService {
 	
 	public Ticket bookTicket(Passenger p);
